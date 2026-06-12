b0d045ffab230073206a37b6bb52286138c454d8
@ddvishal369 <ddvishal369@gmail.com>
2026-06-12 13:50:03 +0200

    Add public API exports and project configuration

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { EyeTracker } from './EyeTracker.js';
+export type { CalibrationData, ElementStats, GazePoint, ModuleEvents, TargetInput } from './types.js';
---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export type TargetInput = HTMLElement[] | string;
+
+export interface GazePoint {
+  x: number;
+  y: number;
+}
+
+export interface CalibrationPoint {
+  x: number;
+  y: number;
+}
+
+export interface CalibrationData {
+  version: number;
+  points: CalibrationPoint[];
+  timestamp: number;
+}
+
+export interface ElementStats {
+  elementId: string;
+  dwellMs: number;
+}
+
+export interface LookEvent {
+  element: HTMLElement;
+}
+
+export interface CurrentElementEvent {
+  element: HTMLElement | null;
+}
+
+export interface ModuleEvents {
+  lookat: LookEvent;
+  lookaway: LookEvent;
+  currentElementChanged: CurrentElementEvent;
+}
