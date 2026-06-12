264e8df406880215d2f86c38f1b0308e2b924c40
Adill00 <138373510+Adill00@users.noreply.github.com>
2026-06-13 00:34:12 +0200

    Revert "Modified and refactored calibration overlay"

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { EyeTracker } from './EyeTracker.js';
-export { CalibrationCancelledError } from './core/CalibrationManager.js';
-export type { CalibrationData, ElementStats, GazePoint, ModuleEvents, TargetInput } from './types.js';
